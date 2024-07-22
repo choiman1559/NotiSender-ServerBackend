c7cec67f6f935a9281a974612200126d4ce811b6
choiman1559 <cuj1559@gmail.com>
2024-07-22 17:29:02 +0000

    Cached Oauth token to enhance speed

--- src/main/java/com/noti/server/process/auth/FirebaseHelper.java
+++ src/main/java/com/noti/server/process/auth/FirebaseHelper.java
@@ -9,8 +9,12 @@ import com.google.firebase.auth.UserRecord;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class FirebaseHelper {
+
+    public static ConcurrentHashMap<String, OauthToken> cachedTokenHashmap = new ConcurrentHashMap<>();
+
     public static void init(String credentialPath) throws IOException {
         FileInputStream serviceAccount = new FileInputStream(credentialPath);
         FirebaseOptions options = FirebaseOptions.builder()
@@ -22,10 +26,23 @@ public class FirebaseHelper {
 
     public static boolean verifyToken(String idToken, String uid) {
         try {
+            if(cachedTokenHashmap.containsKey(uid)) {
+                OauthToken cachedToken = cachedTokenHashmap.get(uid);
+                if(cachedToken.compareToken(idToken) && cachedToken.isStillValid()) {
+                    return true;
+                }
+            }
+
             FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
             FirebaseToken decodedToken = firebaseAuth.verifyIdToken(idToken);
             UserRecord userRecord = firebaseAuth.getUser(decodedToken.getUid());
-            return uid.equals(userRecord.getUid());
+            OauthToken oauthToken = new OauthToken(idToken);
+
+            boolean isValid = uid.equals(userRecord.getUid());
+            if(isValid) {
+                cachedTokenHashmap.put(uid, oauthToken);
+            }
+            return isValid;
         } catch (Exception e) {
             e.printStackTrace();
             return false;
